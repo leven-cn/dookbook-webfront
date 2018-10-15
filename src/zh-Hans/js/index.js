a2a472b2f000f3f9e23dcef3bf1ff7ae8a504f97
leven-cn <leven.cn@gmail.com>
2018-10-15 22:38:23 +0800

    完善首页标题渐变效果

--- src/zh-Hans/js/index.js
+++ src/zh-Hans/js/index.js
@@ -1,39 +1,44 @@
 // 输入框
 var inputBox = document.querySelector('input[type="text"]');
 inputBox.onfocus = function(){
-	this.placeholder = "";
+  this.placeholder = "";
 }
 inputBox.onblur = function(){
-	var searchText = this.value;
-	console.log(searchText);
-	if(searchText){
-		location.href = "cookbook.html?search=" + searchText;
-	}else{
-		this.placeholder = "请开始你的表演 ...";
-	}
+  var searchText = this.value;
+  console.log(searchText);
+  if(searchText){
+    location.href = "cookbook.html?search=" + searchText;
+  }else{
+    this.placeholder = "请开始你的表演 ...";
+  }
 }
 
 
-  // dookbook 字体效果
-var h1Element =  document.querySelector("h1");
+// dookbook 字体效果
 var pElement =  document.querySelector("p");
 var mainElement =  document.querySelector("main");
-var fuzzy =  document.getElementsByClassName("fuzzy");
+function fuzzyWords(){
+  var fuzzy =  document.getElementsByClassName("fuzzy");
+  for(var i=0;i<fuzzy.length;i++){
+    fuzzy[i].style.animation="1.5s fuzzy linear";
+    fuzzy[i].style.animationFillMode="forwards";
+  }
+  setTimeout(function(){
+    for(var i=0;i<fuzzy.length;i++){
+      fuzzy[i].style.display="none"
+    }
 
-for(var i=0;i<fuzzy.length;i++){
-	fuzzy[i].style.animation="1.5s fuzzy linear";
-	fuzzy[i].style.animationFillMode="forwards";
+    setTimeout(function(){
+      var h1 = document.querySelector("h1");
+      h1.style.animation="1.5s fontSize linear";
+      h1.style.animationFillMode="forwards";
+
+      setTimeout(function(){
+        // TODO: 增加渐变效果，跟上述一致
+        pElement.style.display="block";
+        mainElement.style.display="block";
+      }, 500);
+    }, 1000);
+  }, 1550); 
 }
-
-setTimeout(function(){
-	for(var i=0;i<fuzzy.length;i++){
-		fuzzy[i].style.display="none"
-	}
-	h1Element.style.animation="1.5s fontSize linear";
-	h1Element.style.animationFillMode="forwards";
-
-	pElement.style.display="block";
-	mainElement.style.display="block";
-},1550)
-
-
+fuzzyWords();
