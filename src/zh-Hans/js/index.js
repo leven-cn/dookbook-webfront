7956b992a1c700aa77a7e1bfe8974ec2a276a899
Liao Yazhi <1594435860@qq.com>
2018-10-15 23:20:50 +0800

    index效果完善

--- src/zh-Hans/js/index.js
+++ src/zh-Hans/js/index.js
@@ -20,20 +20,35 @@ var pElement =  document.querySelector("p");
 var mainElement =  document.querySelector("main");
 var fuzzy =  document.getElementsByClassName("fuzzy");
 
+// Developers' Cookbook 慢慢模糊
 for(var i=0;i<fuzzy.length;i++){
-	fuzzy[i].style.animation="1.5s fuzzy linear";
+	fuzzy[i].style.animation="1.2s fuzzy linear";
 	fuzzy[i].style.animationFillMode="forwards";
 }
 
+// evelopers' C 宽度慢慢变0
 setTimeout(function(){
 	for(var i=0;i<fuzzy.length;i++){
-		fuzzy[i].style.display="none"
+		fuzzy[i].style.animation="1.2s fuzzyWidth linear";
+	  fuzzy[i].style.animationFillMode="forwards";
 	}
-	h1Element.style.animation="1.5s fontSize linear";
-	h1Element.style.animationFillMode="forwards";
+},1400)
 
+// dookbook 字慢慢变大
+setTimeout(function(){
+	h1Element.style.animation="1.2s fontSize linear";
+	h1Element.style.animationFillMode="forwards";
+},3000)
+
+// 显示开发者日本菜谱和输入框
+setTimeout(function(){
+	document.body.style.display = "block";
+	h1Element.style.marginTop="12.5%";
+	pElement.style.animation="1.2s show linear";
+	mainElement.style.animation="1.2s show linear";
+},5000)
+
+setTimeout(function(){
 	pElement.style.display="block";
 	mainElement.style.display="block";
-},1550)
-
-
+},5100)
