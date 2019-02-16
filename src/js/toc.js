bd45461c11e788de80ba299bb4022596ad70c46b
Li Yun <leven.cn@gmail.com>
2019-02-16 16:56:31 +0800

    Add TOC js

---
+++ src/js/toc.js
@@ -0,0 +1,28 @@
+/**
+ * 左侧TOC菜单
+ */
+const selectedClassName = "active";
+
+var tocList = document.querySelectorAll(".toc>ul>li");
+var selectedMenu = null;  // 当前选中的菜单
+
+for (var i = 0; i < tocList.length; i++) {
+  var menu = tocList[i];
+  if (menu.querySelector("ul")) {
+    menu.querySelector("a").classList.add("fold");
+  }
+
+  // 展开或折叠子菜单
+  menu.onclick = function () {
+    if (!this.classList.contains(selectedClassName)) {
+      // 清空当前选中的菜单样式
+      if (selectedMenu) {
+        selectedMenu.classList.remove(selectedClassName);
+        selectedMenu.querySelector("a").classList.replace("unfold", "fold");
+      }
+      this.classList.add(selectedClassName);
+      selectedMenu = this;
+      this.querySelector("a").classList.replace("fold", "unfold");
+    }
+  }
+}
