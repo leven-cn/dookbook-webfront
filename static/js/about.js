9c221a36374fddcc7d0a3f957bd7d0b496161db8
Li Yun <leven.cn@gmail.com>
2019-08-20 15:47:37 +0800

    minor fix

--- static/js/about.js
+++ static/js/about.js
@@ -2,6 +2,10 @@
  * 关于页面
  */
 
+/**
+  * 获取URL查询参数
+  * @param {String} param
+  */
 function getQueryVariable (param) {
   var query = window.location.search.substring(1)
   var vars = query.split('&')
@@ -12,41 +16,27 @@ function getQueryVariable (param) {
   return false
 }
 
-/**
- * 滚动下箭头消失显示
- */
-function toggleDownIcon (asideUL, downIcon) {
-  asideUL.onmouseover = function () {
-    if (this.scrollTop === 0) {
-      downIcon.style.display = 'inline-block'
-    } else if (this.scrollTop + this.clientHeight === this.scrollHeight) {
-      downIcon.style.display = 'none'
-    }
+// 滚动下箭头消失显示
+var asideUL = document.querySelector('aside ul')
+var downIcon = document.querySelector('.down')
+asideUL.onmouseover = function () {
+  if (this.scrollTop === 0) {
+    downIcon.style.display = 'inline-block'
+  } else if (this.scrollTop + this.clientHeight === this.scrollHeight) {
+    downIcon.style.display = 'none'
   }
 }
 
-/**
- * 服务器消息推送
- */
-function serverNotify (session) {
-  if (session) {
-    var evtSource = new EventSource('/github/oauth/notify/' + session + '/')
-    evtSource.onmessage = function (e) {
-      console.log('message: ' + e.data)
-    }
-    evtSource.addEventListener('github:oauth', function (e) {
-      if (e.data) {
-        alert(e.data)
-      }
-    }, false)
+// 服务器消息推送
+var session = getQueryVariable('session')
+if (session) {
+  var evtSource = new EventSource('/github/oauth/notify/' + session + '/')
+  evtSource.onmessage = function (e) {
+    console.log('message: ' + e.data)
   }
+  evtSource.addEventListener('github:oauth', function (e) {
+    if (e.data) {
+      alert(e.data)
+    }
+  }, false)
 }
-
-document.ready(function () {
-  var asideUL = document.querySelector('aside ul')
-  var downIcon = document.querySelector('.down')
-  toggleDownIcon(asideUL, downIcon)
-
-  var session = getQueryVariable('session')
-  serverNotify(session)
-})
