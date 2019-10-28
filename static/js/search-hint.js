8281ee393d789e1b78a5c725be967897bb991d25
Li Yun <leven.cn@gmail.com>
2019-10-28 19:20:58 +0800

    Improvement

--- static/js/search-hint.js
+++ static/js/search-hint.js
@@ -1,16 +1,21 @@
 /* 创建搜索提示下拉列表 */
-function createSearchHintList(hints) {
-  ulBox = document.createElement("ul")
-
-  for(var i = 0; i < hints.length; i++){
-    ulBox.innerHTML += '<li><a href="#"><span class="search-hints-subject"><img src="'+hints[i].subject_icon_url+'" alt="'+hints[i].subject+'">'+hints[i].subject+'</span><span class="search-hints-topic">'+hints[i].topic+'</span></a></li>'
+function createSearchHintList (hints) {
+  if (ulBox == null) {
+    ulBox = document.createElement('ul')
+    document.getElementById('search').appendChild(ulBox)
   }
-  
 
+  if (hints == null || !hints) {
+    ulBox.innerHTML = '<li><a href="#">暂无搜索结果</a></li>'
+  } else {
+    for (var i = 0; i < hints.length; i++) {
+      ulBox.innerHTML += '<li><a href="#"><span class="search-hints-subject"><img src="' + hints[i].subject_icon_url + '" alt="' + hints[i].subject + '">' + hints[i].subject + '</span><span class="search-hints-topic">' + hints[i].topic + '</span></a></li>'
+    }
+  }
 }
 
-// 获取搜索提示列表
-function fetchSearchHintList() {
+/* 获取搜索提示列表 */
+function fetchSearchHintList () {
   var xhr = new XMLHttpRequest()
   xhr.open('GET', '/static/js/text.json', true)
   xhr.send()
@@ -18,49 +23,53 @@ function fetchSearchHintList() {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         var data = JSON.parse(xhr.responseText)
-        createSearchHintList(data)
-        document.getElementById("search").appendChild(ulBox)
+        return data.hints
+      } else {
+        console.error('请求接口失败, status=' + xhr.status)
+        return null
       }
+    } else {
+      console.error('请求接口失败')
+      return null
     }
   }
 }
 
-// 显示搜索下拉列表
-function showSearchHintList() {
+/* 显示搜索下拉列表 */
+function showSearchHintList () {
   if (ulBox != null) {
-    ulBox.style.display = "block"
+    ulBox.style.display = 'block'
   } else {
-    fetchSearchHintList()
+    createSearchHintList(fetchSearchHintList())
   }
 }
 
 var ulBox = null
 var input = document.querySelector('input')
 
-//点击输入框,清空输入框内文字，并且列出下拉列表
-input.onclick = function(event){
+// 点击输入框,清空输入框内文字，并且列出下拉列表
+input.onclick = function (event) {
   event.stopPropagation()
 
   if (!this.value) {
     this.placeholder = ''
   } else {
-    ulBox.style.display = "block"
+    ulBox.style.display = 'block'
   }
 }
 
-input.oninput = function(){
-  if(!this.value){
-    ulBox.style.display = "none"
+input.oninput = function () {
+  if (!this.value) {
+    ulBox.style.display = 'none'
     return
   }
   showSearchHintList()
 }
 
-//点击页面其他地方，隐藏下拉列表
-document.body.onclick = function(e){
+// 点击页面其他地方，隐藏下拉列表
+document.body.onclick = function (e) {
   if (input.placeholder === '') {
-    var prompt = input.getAttribute('data-prompt') // 获取data-prompt 属性的值
-    input.placeholder = prompt
+    input.placeholder = input.getAttribute('data-prompt')
   }
   if (ulBox != null) {
     ulBox.style.display = 'none'
