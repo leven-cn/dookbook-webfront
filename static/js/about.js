262452cb67cc4b0fa4646093e1692663e7118009
Li Yun <leven.cn@gmail.com>
2019-10-31 09:25:23 +0800

    修复关于页无法搜索的BUG fix #60

--- static/js/about.js
+++ static/js/about.js
@@ -2,6 +2,45 @@
  * 关于页面
  */
 
+var input = document.querySelector('input')
+var ulBox = null
+var inputSilence = false
+
+initInput(input)
+
+// 点击输入框
+input.onclick = function (event) {
+  event.stopPropagation()
+
+  if (!this.value) {
+    this.placeholder = ''
+  } else {
+    showSearchHintList()
+  }
+}
+
+// 处理搜索输入
+input.oninput = function () {
+  if (!this.value) {
+    if (ulBox) {
+      ulBox.style.display = 'none'
+    }
+    return
+  }
+
+  if (!inputSilence) {
+    var pos = (location.pathname.startsWith('/about')) ? 2 : 1
+    var lang = location.pathname.split('/')[pos]
+    if (lang) {
+      lang = lang.toLowerCase()
+      lang = (lang !== 'en' && lang !== 'zh-hans') ? 'en' : lang
+    } else {
+      lang = 'en'
+    }
+    fetchSearchHintList(this.value.trim(), lang)
+  }
+}
+
 // 滚动下箭头消失显示
 var asideUL = document.querySelector('aside ul')
 var downIcon = document.querySelector('.down')
