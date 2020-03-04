2f898e98535db8ab881be76dfc7bea576c8de07e
leven-cn <leven.cn@gmail.com>
2020-03-04 17:28:17 +0800

    简化搜索提示JS

--- static/js/about.js
+++ static/js/about.js
@@ -2,51 +2,11 @@
  * 关于页面
  */
 
-var input = document.querySelector('input')
-var ulBox = null
-const INPUT_SILENCE_TIME = 100  // in microseconds
-var inputSilence = false
+var searchInput = document.querySelector('input')
+var searchList = document.createElement('ul')
+document.getElementById('search').appendChild(searchList)
 
-initInput(input)
-
-// 点击输入框
-input.onclick = function (event) {
-  event.stopPropagation()
-
-  if (!this.value) {
-    this.placeholder = ''
-  } else {
-    showSearchHintList()
-  }
-}
-
-// 处理搜索输入
-input.oninput = function () {
-  if (!this.value) {
-    if (ulBox) {
-      ulBox.style.display = 'none'
-    }
-    return
-  }
-
-  if (!inputSilence) {
-    var pos = (location.pathname.startsWith('/about')) ? 2 : 1
-    var lang = location.pathname.split('/')[pos]
-    if (lang) {
-      lang = lang.toLowerCase()
-      lang = (lang !== 'en' && lang !== 'zh-hans') ? 'en' : lang
-    } else {
-      lang = 'en'
-    }
-    fetchSearchHintList(this.value.trim(), lang)
-
-    /* 设置阻断事件间隔，过滤过于频繁的请求 */
-    inputSilence = true
-    setTimeout(function () {
-      inputSilence = false
-    }, INPUT_SILENCE_TIME)
-  }
-}
+initSearch(searchInput, searchList)
 
 // 滚动下箭头消失显示
 var asideUL = document.querySelector('aside ul')
