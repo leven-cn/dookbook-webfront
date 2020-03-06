6861214f921f63e805d3343e9dfc954ea977363f
Liao Yazhi <1594435860@qq.com>
2020-03-06 14:04:21 +0800

    适配Safari

--- static/js/content.js
+++ static/js/content.js
@@ -5,3 +5,12 @@ var searchList = document.createElement('ul')
 document.getElementById('search').appendChild(searchList)
 
 initSearch(searchInput, searchList)
+
+// 判断浏览器为Safari执行的样式
+var issafariBrowser = /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);;
+if (issafariBrowser) {
+  var meter = document.getElementsByTagName("meter")
+  for (var i = 0; i < meter.length; i++ ) {
+    meter[i].style.webkitAppearance = "none"
+  }
+}
