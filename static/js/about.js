9df05b8f1c19574c5ba2b4571f348cb58a353866
leven-cn <leven.cn@gmail.com>
2020-03-07 12:32:38 +0800

    优化服务器消息推送

--- static/js/about.js
+++ static/js/about.js
@@ -2,33 +2,31 @@
  * 关于页面
  */
 
+/**
+ * 滚动下箭头消失显示
+ */
+function styleDown() {
+  var asideUL = document.querySelector('aside ul')
+  var downIcon = document.querySelector('.down')
+  asideUL.onmouseover = function () {
+    if (this.scrollTop === 0) {
+      downIcon.style.display = 'inline-block'
+    } else if (this.scrollTop + this.clientHeight === this.scrollHeight) {
+      downIcon.style.display = 'none'
+    }
+  }
+}
+
 var searchInput = document.querySelector('input')
 var searchList = document.createElement('ul')
 document.getElementById('search').appendChild(searchList)
 
 initSearch(searchInput, searchList)
+styleDown()
 
-// 滚动下箭头消失显示
-var asideUL = document.querySelector('aside ul')
-var downIcon = document.querySelector('.down')
-asideUL.onmouseover = function () {
-  if (this.scrollTop === 0) {
-    downIcon.style.display = 'inline-block'
-  } else if (this.scrollTop + this.clientHeight === this.scrollHeight) {
-    downIcon.style.display = 'none'
-  }
-}
-
-// 服务器消息推送
 var session = getQueryVariable('session')
 if (session) {
-  var evtSource = new EventSource('/helper/github/oauth/notify/' + session + '/')
-  evtSource.onmessage = function (e) {
-    console.log('message: ' + e.data)
-  }
-  evtSource.addEventListener('github:oauth', function (e) {
-    if (e.data) {
-      alert(e.data)
-    }
-  }, false)
+  serverEvent('/helper/github/oauth/notify/' + session + '/', 'github:oauth', function(data) {
+    alert(data)
+  })
 }
