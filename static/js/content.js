474f7ff9eb44946802106a8fef3e05200f5e595f
leven-cn <leven.cn@gmail.com>
2020-03-07 12:11:53 +0800

    minor fix

--- static/js/content.js
+++ static/js/content.js
@@ -7,8 +7,8 @@ document.getElementById('search').appendChild(searchList)
 initSearch(searchInput, searchList)
 
 // 判断浏览器为Safari执行的样式
-var issafariBrowser = /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);;
-if (issafariBrowser) {
+var isSafariBrowser = /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+if (isSafariBrowser) {
   var meter = document.getElementsByTagName("meter")
   for (var i = 0; i < meter.length; i++ ) {
     meter[i].style.webkitAppearance = "none"
