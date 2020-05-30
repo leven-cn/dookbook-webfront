a014200fa9f5256c97f65abee33fd96c52738a1e
Liao Yazhi <1594435860@qq.com>
2020-05-30 13:24:15 +0800

    完善表格样式

--- static/js/wikipedia.js
+++ static/js/wikipedia.js
@@ -8,4 +8,12 @@ for (var i = 0; i < allTables.length; i++) {
   if (allTables[i].querySelector('figure')) {
     addClass(allTables[i], 'table-img')
   }
+
+  var allTableTrs = allTables[i].querySelectorAll('tr')
+
+  for (var j = 0; j < allTableTrs.length; j++) {
+    if (allTableTrs[j].querySelectorAll('td').length == 1) {
+      addClass(allTableTrs[j].querySelector('td'), 'first-width')
+    }
+  }
 }
